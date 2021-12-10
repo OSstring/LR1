bc62e8f7841de42ba72568f69e2be5a852682c16
OSstring <95416549+OSstring@users.noreply.github.com>
2021-12-10 22:30:43 +0300

    Добавьте файлы проекта.

---
+++ CommandEntities.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace LR1
+{
+    public class CommandEntity
+    {
+
+        [Key] //Следующее поле/свойство будет первичным ключом в таблице
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)] //Способ автогенерации ключа
+        public int CommandId { get; set; } // ИД команды
+        public string? CommandTrigger { get; set; } // Слово, на которое команда будет вызываться
+        public string? SourceNames { get; set; } // Список источников, откуда команда может приходить
+        public string? CommandAnswer { get; set; } // Ответ команды
+        public Boolean IsScript { get; set; } // Является ли скриптом
+        public String? ScriptName { get; set; } // Название скрипта
+        public DateTime Date { get; set; }
+        public String? Description { get; set; }
+        public String? Autor { get; set; }
+    }
+    
+}
---
+++ DatabaseContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace LR1
+{
+    public class DatabaseContext : DbContext //Наследуемся от DBContext
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options) // Не забываем звать основной конструктор 
+        {
+        }
+
+        public DbSet<CommandEntity> CommandEntities { get; set; } //Объявляем таблицу для сущности
+    }
+}
+
